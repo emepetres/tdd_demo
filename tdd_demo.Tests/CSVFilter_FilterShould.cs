74f0246f42114f9d779e4fbea556d4f97a02bc85
Javier Carnero <jcarnero@plainconcepts.com>
2022-12-05 22:26:03 +0100

    Filter IVA/ICIG exclusive

--- tdd_demo.Tests/CSVFilter_FilterShould.cs
+++ tdd_demo.Tests/CSVFilter_FilterShould.cs
@@ -1,14 +1,57 @@
 ﻿using Xunit;
 using tdd_demo;
+using System.Collections.Generic;
 
 namespace tdd_demo.UnitTests.CSVFilter
 {
     public class CSVFilter_FilterShould
     {
-        [Fact]
-        public void Filter_HeaderNotPresent_ReturnNull()
+        private static string[] BuildCSV(string line)
         {
-            tdd_demo.CSVFilter.Filter(new string[] { "bad header" });
+            var csv = new List<string>();
+            csv.Add("Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente");
+
+            csv.Add(line);
+
+            return csv.ToArray();
+        }
+
+        private static string[] BuildCSV(string[] lines)
+        {
+            var csv = new List<string>();
+            csv.Add("Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente");
+            
+            foreach(var line in lines)
+            {
+                csv.Add(line);
+            }
+            
+            return csv.ToArray();
+        }
+
+        [Fact]
+        public void FilterAll_HeaderNotPresent_ReturnNull()
+        {
+            tdd_demo.CSVFilter.FilterAll(new string[] { "bad header" });
+        }
+
+        [Theory]
+        [InlineData("1,02/05/2019,1000,810,19,,ACER Laptop,B76430134,")]
+        [InlineData("2,03/08/2019,2000,2000,,8,MacBook Pro,,78544372A")]
+        public void IsValid_OneLine_IsValid(string csv_line)
+        {
+            var is_valid = tdd_demo.CSVFilter.IsValid(csv_line);
+
+            Assert.True(is_valid);
+        }
+
+        [Theory]
+        [InlineData("1,02/05/2019,1000,810,19,8,ACER Laptop,B76430134,")]
+        public void IsValid_IVA_IGIC_Exclusive(string csv_line)
+        {
+            var is_valid = tdd_demo.CSVFilter.IsValid(csv_line);
+
+            Assert.False(is_valid);
         }
     }
 }
--- tdd_demo/CSVFilter.cs
+++ tdd_demo/CSVFilter.cs
@@ -7,12 +7,39 @@ using System.Threading.Tasks;
 
 namespace tdd_demo
 {
+    struct CSVLine
+    {
+        public string num;
+        public string fecha;
+        public string bruto;
+        public string neto;
+        public string iva;
+        public string igic;
+        public string concepto;
+        public string cif;
+        public string nif;
+
+        public CSVLine(string line)
+        {
+            var data = line.Split(",");
+            num = data[0];
+            fecha = data[1];
+            bruto = data[2];
+            neto = data[3];
+            iva = data[4];
+            igic = data[5];
+            concepto = data[6];
+            cif = data[7];
+            nif = data[8];
+        }
+    }
+    
     public static class CSVFilter
     {
         private static readonly Regex sWhitespace = new Regex(@"\s+");
         private static string HEADER = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente";
         
-        public static string[]? Filter(string[] input)
+        public static string[]? FilterAll(string[] input)
         {
             var inputs = new Queue<string>(input);
 
@@ -31,10 +58,22 @@ namespace tdd_demo
 
             var filtered = new List<string>();
             filtered.Add(HEADER);
-            // filter each line
-
+            while(inputs.TryDequeue(out input_line!))
+            {
+                if (IsValid(input_line))
+                {
+                    filtered.Add(input_line);
+                }
+            }
 
             return filtered.ToArray();
+        }
+
+        public static bool IsValid(string line)
+        {
+            var data = new CSVLine(line);
+
+            return (data.iva == String.Empty || data.igic == String.Empty);
         }
 
         private static string CleanHeader(string header)
