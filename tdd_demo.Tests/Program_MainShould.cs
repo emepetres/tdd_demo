9d195945462ba3a7cabc0746c0130ff77adbbcb2
Javier Carnero <jcarnero@plainconcepts.com>
2022-12-05 20:11:10 +0100

    refactored main tests as a theory

--- tdd_demo.Tests/Program_MainShould.cs
+++ tdd_demo.Tests/Program_MainShould.cs
@@ -6,37 +6,14 @@ namespace tdd_demo.UnitTests
 {
     public class Program_MainShould
     {
-        [Fact]
-        public void Main_NoInputs_ExitNonZero()
+        [Theory]
+        [InlineData(0, new string[] { } )]
+        [InlineData(0, new string[] { "--csv" })]
+        [InlineData(0, new string[] { "--other", "c:/test.csv" })]
+        [InlineData(0, new string[] { "--csv", "c:/test.csv" })]
+        [InlineData(0, new string[] { "--csv", "./Resources/bad_format.txt" })]
+        public void Main_WrongInput_ExitNonZero(int _, string[] args)
         {
-            Assert.True(Program.Main(new string[] { }) != 0);
-        }
-
-        [Fact]
-        public void Main_NoCSVInput_ExitNonZero()
-        {
-            var args = new string[] { "--other", "c:/test.csv" };
-            Assert.True(Program.Main(args) != 0);
-        }
-
-        [Fact]
-        public void Main_CSVMissing_ExitNonZero()
-        {
-            var args = new string[] { "--csv" };
-            Assert.True(Program.Main(args) != 0);
-        }
-
-        [Fact]
-        public void Main_CSVNotExists_ExitNonZero()
-        {
-            var args = new string[] { "--csv", "c:/test.csv" };
-            Assert.True(Program.Main(args) != 0);
-        }
-
-        [Fact]
-        public void Main_InputIsNotCSV_ExitNonZero()
-        {
-            var args = new string[] { "--csv", "./Resources/bad_format.txt" };
             Assert.True(Program.Main(args) != 0);
         }
     }
